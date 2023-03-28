45ace58d86eb6024c95734072a728ae051c34b39
JacquesSmit <JacquesSmit@LT-JSM>
2023-03-28 14:11:09 +0200

    Add project files.

---
+++ EmpowerDemo2_JSM_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDemo2_JSM_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDemo2_JSM_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("A8D2D850-7F56-4019-8A15-C1F9F811D373")]
+[assembly: AssemblyVersion("1.0.0.0")]
